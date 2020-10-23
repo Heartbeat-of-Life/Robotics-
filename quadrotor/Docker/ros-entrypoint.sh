163c9b760165ebdf278ab8fdee1e9e9dd0e8d573
Maximilian J. Graf <maximilian_1989@gmx.de>
2020-10-23 17:55:38 +0200

    update -- dockerfile + start up script

---
+++ quadrotor/Docker/ros-entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+source /opt/ros/noetic/setup.sh
+exec "$@"
--- quadrotor/start_container.sh
+++ quadrotor/start_container.sh
@@ -1,2 +1,20 @@
-docker run -ti --rm --user $UID:$GID -v /etc/passwd:/etc/passwd --mount type=bind,source=/home,target=/home -v /etc:/etc  ros:base /$SHELL
+IMG_NAME=ros:base
+CONTAINER_NAME=robotics_sandbox
 
+if [[ -z $(docker ps --filter "name=$CONTAINER_NAME" | grep $CONTAINER_NAME) ]]
+then
+
+	ARGS=(" -ti
+		--rm
+		--user $UID:$GID
+		-v /etc/passwd:/etc/passwd
+		-e TERM=$TERM
+		--name $CONTAINER_NAME
+		--mount type=bind,source=/home,target=/home")
+
+
+	echo "Starting new docker container"
+	docker run $ARGS $IMG_NAME $SHELL 
+fi
+
+	docker exec -ti $CONTAINER_NAME /usr/bin/ros-entrypoint.sh $SHELL
