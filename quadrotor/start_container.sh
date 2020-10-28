b65b54de1079c7d1037ecca274f75de12e20702e
Maximilian J. Graf <maximilian_1989@gmx.de>
2020-10-28 22:21:14 +0100

    update start_container.sh

--- quadrotor/start_container.sh
+++ quadrotor/start_container.sh
@@ -10,18 +10,24 @@ then
 		-v /etc/sudoers:/etc/sudoers:ro
 		-v /etc/group:/etc/group:ro
 		-v /etc/shadow:/etc/shadow:ro
+		-v /tmp/.X11-unix:/tmp/.X11-unix
+		-v /run/user/$UID:/run/user/$UID
 		--name $CONTAINER_NAME
+		-w $(pwd)
+		-e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR
 		--mount type=bind,source=/home,target=/home")
 
 
 	echo "Starting new docker container"
 	docker run $ARGS $IMG_NAME /usr/bin/ros-entrypoint.sh  $SHELL 
 else
-	echo "Docker container already running"
+	echo "Docker container already running. I will attach to running container"
 fi
 
 	ARGS=(" -ti
 		--user $UID:$GID
-		-e TERM=$TERM")
-# usermod -aG $USER sudo?
-	docker exec $ARGS $CONTAINER_NAME /usr/bin/ros-entrypoint.sh   $SHELL 
+		-e TERM=$TERM
+		-e DISPLAY=unix$DISPLAY
+		-w $(pwd)")
+
+		docker exec $ARGS $CONTAINER_NAME /usr/bin/ros-entrypoint.sh  $SHELL 
