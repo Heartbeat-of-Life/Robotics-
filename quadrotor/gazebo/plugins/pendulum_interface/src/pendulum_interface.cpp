4c51f4b001a68d4ce718099337e43c7cc95fdc41
Maximilian J. Graf <maximilian_1989@gmx.de>
2021-01-27 22:27:55 +0100

    [FIX] get wagon mass + limits

--- quadrotor/gazebo/plugins/pendulum_interface/src/pendulum_interface.cpp
+++ quadrotor/gazebo/plugins/pendulum_interface/src/pendulum_interface.cpp
@@ -3,6 +3,7 @@
 
 #include "gazebo/physics/Joint.hh"
 #include "gazebo/physics/JointState.hh"
+#include "gazebo/physics/PhysicsTypes.hh"
 #include "ignition/math/Angle.hh"
 #include "rclcpp/rclcpp.hpp"
 #include <std_msgs/msg/float64.hpp>
@@ -30,10 +31,17 @@ namespace gazebo
 			const  ignition::math::v6::Pose3<double>pose = this->wagon->WorldPose();
 //			res->x=this->wagon->WorldPose().Pos().Y();
 			res->x =this->wagon_rail_joint->Position();
-			res->dx=this->wagon_rail_joint->GetVelocity(0);
-			std::cout <<"positon wagon="<<res->x<<" v="<< res->dx<<" v2="<<this->wagon_rail_joint->GetVelocity(0) <<" limit="<<this->wagon_rail_joint->LowerLimit()<<" "<<this->wagon_rail_joint->UpperLimit()<<std::endl;
+			const static double pos = this->wagon->WorldPose().Pos().Y();
+			
+//			std::cout <<"wr pose="<<this->wagon_rail_joint->Position()<<" pos= "<<pos <<std::endl;
+//			std::cout <<"wagon="<< this->wagon->RelativePose().Pos().Y() <<std::endl;
+//			std::cout<<"relative pos wagon on rail="<<this->wagon->RelativePose().Pos().X()<<" "<<this->wagon->RelativePose().Pos().Y()<< " worldpose wagon="<<this->wagon->WorldPose().Pos().Y()<<std::endl;
+//			res->dx=this->wagon_rail_joint->GetVelocity(0);
+//			std::cout <<"positon wagon="<<res->x<<" v="<< res->dx<<" v2="<<this->wagon_rail_joint->GetVelocity(0) <<" limit="<<this->wagon_rail_joint->LowerLimit()<<" "<<this->wagon_rail_joint->UpperLimit()<<std::endl;
 			res->phi = this->model->GetLink("pendulum_stick")->WorldPose().Rot().Roll();
 			res->dphi= this->pendulum_joint->GetVelocity(0);
+			std::cout<<"wagon masss=="<<this->wagon->GetInertial()->Mass()<<std::endl;
+//			std::cout<<"platelen="<<this->wagon->GetVisualMessage()<<std::endl;
 			return;
 
 		};
@@ -56,6 +64,18 @@ namespace gazebo
 		this->pendulum_joint = model->GetJoint("pendulum_to_wagon");
 		this->wagon_rail_joint = model->GetJoint("table_plate_to_wagon");
 		this->update_connection=event::Events::ConnectWorldUpdateBegin(std::bind(&PendulumSensor::onUpdate,this));
+		this->mass = this->wagon->GetInertial()->Mass();
+		this->wagon_length = this->wagon->BoundingBox().YLength();
+
+		physics::LinkPtr left_leg = this->model->GetLink("table_leg_left");
+		physics::LinkPtr table_plate = this->model->GetLink("table_plate");
+		const double xmin = left_leg->BoundingBox().YLength()/2+this->wagon_length/2;
+		const double xmax = table_plate->BoundingBox().YLength()-xmin;
+		std::cout <<"xmin="<<xmin<<" xmax="<<xmax<<std::endl;
+
+
+
+
 		if(!rclcpp::ok())
 		{
 			rclcpp::init(0,nullptr);
@@ -84,6 +104,9 @@ namespace gazebo
 	private: rclcpp::Clock clock;
 	private: event::ConnectionPtr update_connection;
 	private: double current_force;
+	private: double mass;
+	private: double wagon_length;
+	
 
 };
 	GZ_REGISTER_MODEL_PLUGIN(PendulumSensor)
